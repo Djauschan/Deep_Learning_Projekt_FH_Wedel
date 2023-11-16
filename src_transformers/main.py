38e5bbd37acb001c9357c9183e1317c72a8ecdd5
Luca Nickel <luca.nickel@pm.me>
2023-11-16 11:02:17 +0100

    Adding short command line arguments and more help to the argument parser in main, commented out the code that failed main.py

--- src_transformers/main.py
+++ src_transformers/main.py
@@ -1,13 +1,12 @@
 import argparse
 from typing import Final
-from src_transformers.preprocessing.txtReader import DataReader
-from src_transformers.preprocessing.perSymbolDataset import PerSymbolDataset
-import os
 
 import yaml
 
-from src_transformers.pipelines.trainer import Trainer
 from src_transformers.pipelines.old_nettrainer import NetTrainer
+from src_transformers.pipelines.trainer import Trainer
+from src_transformers.preprocessing.perSymbolDataset import PerSymbolDataset
+from src_transformers.preprocessing.txtReader import DataReader
 
 TRAIN_COMMAND: Final[str] = "train"
 # Second version to work parallel (niklas / luca) -> to be joint
@@ -25,12 +24,18 @@ def setup_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--config",
+        "-c",
         type=str,
         required=True,
-        help="Path to the configuration file with specified options.",
+        help="Path to the configuration file with the specified pipeline options.",
     )
-    parser.add_argument("pipeline", choices=[
-                        TRAIN_COMMAND, TRAIN_COMMAND_NK, PREDICT_COMMAND])
+    parser.add_argument(
+        "--pipeline",
+        "-p",
+        required=True,
+        choices=[TRAIN_COMMAND, TRAIN_COMMAND_NK, PREDICT_COMMAND],
+        help="Pipeline to be started",
+    )
 
     return parser
 
@@ -39,6 +44,10 @@ def main() -> None:
     """
     The main function of the script. It sets up the parser, reads the configuration file,
     and starts the training or prediction process based on the provided pipeline argument.
+
+    Usage examples:
+        python -m src_transformers.main --config data/test_configs/training_config.yaml --pipeline train
+        python -m src_transformers.main -c data/test_configs/training_config.yaml -p train
     """
     parser = setup_parser()
     args, _ = parser.parse_known_args()
@@ -50,14 +59,14 @@ def main() -> None:
     # TODO: Load Optimizer (could be part of training config)
 
     # Create dataset
-    txt_reader = DataReader()
-    data = txt_reader.read_next_txt()
-    dataset = PerSymbolDataset(data, txt_reader.symbols)
+    # txt_reader = DataReader()
+    # data = txt_reader.read_next_txt()
+    # dataset = PerSymbolDataset(data, txt_reader.symbols)
     # Create data loader
     # dataloader = DataLoader( dataset, batch_size=config["BATCH_SIZE"], shuffle=False)
 
-    config["dataset"] = dataset
-    print(config)
+    # config["dataset"] = dataset
+    # print(config)
 
     if args.pipeline == TRAIN_COMMAND:
         Trainer.start_training(**config)
@@ -65,19 +74,29 @@ def main() -> None:
         # niklas' version
 
         # TODO: Put into main function
-        with open("C:/Users/nikla/OneDrive/Uni/23WS/Projekt/Deep_Learning/data/training_configs/training_config.yaml", "r", encoding="utf-8") as f:
+        with open(
+            "C:/Users/nikla/OneDrive/Uni/23WS/Projekt/Deep_Learning/data/training_configs/training_config.yaml",
+            "r",
+            encoding="utf-8",
+        ) as f:
             config_tr = yaml.safe_load(f)
-        with open("C:/Users/nikla/OneDrive/Uni/23WS/Projekt/Deep_Learning/data/model_configs/slp_config.yaml", "r", encoding="utf-8") as f:
+        with open(
+            "C:/Users/nikla/OneDrive/Uni/23WS/Projekt/Deep_Learning/data/model_configs/slp_config.yaml",
+            "r",
+            encoding="utf-8",
+        ) as f:
             config_mdl = yaml.safe_load(f)
 
         # Initialize nettrainer with parameters from config
-        trainer = NetTrainer(**config_tr['trainer_parameters'],
-                             model_name=config_mdl['model_name'],
-                             model_parameters=config_mdl['model_parameters'])
+        trainer = NetTrainer(
+            **config_tr["trainer_parameters"],
+            model_name=config_mdl["model_name"],
+            model_parameters=config_mdl["model_parameters"]
+        )
         # TODO To be discussed: two configs, option: pass model config
 
         # Start training
-        trainer.train(epochs=config['epochs'])
+        trainer.train(epochs=config["epochs"])
     else:
         print("placeholder for predict")
 
--- src_transformers/pipelines/constants.py
+++ src_transformers/pipelines/constants.py
@@ -1,8 +1,7 @@
-
 from typing import Final
 
-from src_transformers.models.transformer import Transformer
 from src_transformers.models.single_layer_perceptron import Single_layer_perceptron
+from src_transformers.models.transformer import Transformer
 
 MODEL_NAME_MAPPING: Final[dict[str, any]] = {"transformer": Transformer}
 # MODEL_NAME_MAPPING: Final[dict[str, any]] = {"test_slp": Single_layer_perceptron}
