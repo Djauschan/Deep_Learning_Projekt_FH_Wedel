a42fe6b5b1f4e4ac3de5639c22c79c310476cddb
Luca Nickel <luca.nickel@pm.me>
2023-12-01 11:30:00 +0100

    Deleted deprecated code and configs, adjusted constants.py imports

--- src_transformers/pipelines/constants.py
+++ src_transformers/pipelines/constants.py
@@ -1,9 +1,7 @@
 from typing import Final
 
-from src_transformers.models.single_layer_perceptron import SingleLayerPerceptron
 from src_transformers.models.transformer import Transformer
 
 MODEL_NAME_MAPPING: Final[dict[str, any]] = {
-    "transformer": Transformer,
-    "single_layer_perceptron": SingleLayerPerceptron
+    "transformer": Transformer
 }
