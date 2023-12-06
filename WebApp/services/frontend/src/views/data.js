aeb634c7c19ce74cdeed411a008d5eb233a98651
ThomasParchmann <150282079+ThomasParchmann@users.noreply.github.com>
2023-12-06 09:15:24 +0100

    Add files via upload

--- WebApp/services/frontend/src/views/data.js
+++ WebApp/services/frontend/src/views/data.js
@@ -1,407 +1,163 @@
-export let basicData = {
-  chart: {
-    type: "spline",
+export const dataSource = [
+  {
+    date: new Date(1994, 2, 1),
+    l: 24.0,
+    h: 25.0,
+    o: 25.0,
+    c: 24.875,
   },
-  title: {
-    text: "Stock Price",
+  {
+    date: new Date(1994, 2, 2),
+    l: 23.625,
+    h: 25.125,
+    o: 24.0,
+    c: 24.875,
   },
-  subtitle: {
-    text: "Price of the Stock",
+  {
+    date: new Date(1994, 2, 3),
+    l: 26.25,
+    h: 28.25,
+    o: 26.75,
+    c: 27.0,
   },
-  xAxis: {
-    categories: [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ],
+  {
+    date: new Date(1994, 2, 4),
+    l: 26.5,
+    h: 27.875,
+    o: 26.875,
+    c: 27.25,
   },
-  yAxis: {
-    title: {
-      text: "Price",
-    },
-    labels: {
-      formatter: function () {
-        return this.value + "$";
-      },
-    },
+  {
+    date: new Date(1994, 2, 7),
+    l: 26.375,
+    h: 27.5,
+    o: 27.375,
+    c: 26.75,
   },
-  tooltip: {
-    crosshairs: true,
-    shared: true,
+  {
+    date: new Date(1994, 2, 8),
+    l: 25.75,
+    h: 26.875,
+    o: 26.75,
+    c: 26.0,
   },
-  credits: {
-    enabled: false,
+  {
+    date: new Date(1994, 2, 9),
+    l: 25.75,
+    h: 26.75,
+    o: 26.125,
+    c: 26.25,
   },
-  plotOptions: {
-    spline: {
-      marker: {
-        radius: 4,
-        lineColor: "#666666",
-        lineWidth: 1,
-      },
-    },
+  {
+    date: new Date(1994, 2, 10),
+    l: 25.75,
+    h: 26.375,
+    o: 26.375,
+    c: 25.875,
   },
-  series: [],
-};
-
-export let asyncData = {
-  name: "Amazon",
-  marker: {
-    symbol: "square",
+  {
+    date: new Date(1994, 2, 11),
+    l: 24.875,
+    h: 26.125,
+    o: 26.0,
+    c: 25.375,
   },
-  data: [
-    7.0,
-    6.9,
-    9.5,
-    14.5,
-    18.2,
-    21.5,
-    25.2,
-    {
-      y: 26.5,
-      marker: {
-        symbol: "url(http://www.highcharts.com/demo/gfx/sun.png)",
-      },
-    },
-    23.3,
-    18.3,
-    13.9,
-    9.6,
-  ],
-};
-
-// area chart data
-export const AreaData = {
-  chart: {
-    type: "area",
+  {
+    date: new Date(1994, 2, 14),
+    l: 25.125,
+    h: 26.0,
+    o: 25.625,
+    c: 25.75,
   },
-  title: {
-    text: "US and USSR nuclear stockpiles",
+  {
+    date: new Date(1994, 2, 15),
+    l: 25.875,
+    h: 26.625,
+    o: 26.125,
+    c: 26.375,
   },
-  subtitle: {
-    text:
-      'Source: <a href="http://thebulletin.metapress.com/content/c4120650912x74k7/fulltext.pdf">' +
-      "thebulletin.metapress.com</a>",
+  {
+    date: new Date(1994, 2, 16),
+    l: 26.25,
+    h: 27.375,
+    o: 26.25,
+    c: 27.25,
   },
-  xAxis: {
-    allowDecimals: false,
-    labels: {
-      formatter: function () {
-        return this.value; // clean, unformatted number for year
-      },
-    },
+  {
+    date: new Date(1994, 2, 17),
+    l: 26.875,
+    h: 27.25,
+    o: 27.125,
+    c: 26.875,
   },
-  yAxis: {
-    title: {
-      text: "Nuclear weapon states",
-    },
-    labels: {
-      formatter: function () {
-        return this.value / 1000 + "k";
-      },
-    },
+  {
+    date: new Date(1994, 2, 18),
+    l: 26.375,
+    h: 27.125,
+    o: 27.0,
+    c: 27.125,
   },
-  tooltip: {
-    pointFormat:
-      "{series.name} produced <b>{point.y:,.0f}</b><br/>warheads in {point.x}",
+  {
+    date: new Date(1994, 2, 21),
+    l: 26.75,
+    h: 27.875,
+    o: 26.875,
+    c: 27.75,
   },
-  plotOptions: {
-    area: {
-      pointStart: 1940,
-      marker: {
-        enabled: false,
-        symbol: "circle",
-        radius: 2,
-        states: {
-          hover: {
-            enabled: true,
-          },
-        },
-      },
-    },
+  {
+    date: new Date(1994, 2, 22),
+    l: 26.75,
+    h: 28.375,
+    o: 27.5,
+    c: 27.0,
   },
-  series: [
-    {
-      name: "USA",
-      data: [
-        null,
-        null,
-        null,
-        null,
-        null,
-        6,
-        11,
-        32,
-        110,
-        235,
-        369,
-        640,
-        1005,
-        1436,
-        2063,
-        3057,
-        4618,
-        6444,
-        9822,
-        15468,
-        20434,
-        24126,
-        27387,
-        29459,
-        31056,
-        31982,
-        32040,
-        31233,
-        29224,
-        27342,
-        26662,
-        26956,
-        27912,
-        28999,
-        28965,
-        27826,
-        25579,
-        25722,
-        24826,
-        24605,
-        24304,
-        23464,
-        23708,
-        24099,
-        24357,
-        24237,
-        24401,
-        24344,
-        23586,
-        22380,
-        21004,
-        17287,
-        14747,
-        13076,
-        12555,
-        12144,
-        11009,
-        10950,
-        10871,
-        10824,
-        10577,
-        10527,
-        10475,
-        10421,
-        10358,
-        10295,
-        10104,
-      ],
-    },
-    {
-      name: "USSR/Russia",
-      data: [
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        null,
-        5,
-        25,
-        50,
-        120,
-        150,
-        200,
-        426,
-        660,
-        869,
-        1060,
-        1605,
-        2471,
-        3322,
-        4238,
-        5221,
-        6129,
-        7089,
-        8339,
-        9399,
-        10538,
-        11643,
-        13092,
-        14478,
-        15915,
-        17385,
-        19055,
-        21205,
-        23044,
-        25393,
-        27935,
-        30062,
-        32049,
-        33952,
-        35804,
-        37431,
-        39197,
-        45000,
-        43000,
-        41000,
-        39000,
-        37000,
-        35000,
-        33000,
-        31000,
-        29000,
-        27000,
-        25000,
-        24000,
-        23000,
-        22000,
-        21000,
-        20000,
-        19000,
-        18000,
-        18000,
-        17000,
-        16000,
-      ],
-    },
-  ],
-};
-
-// pie chart data
-export const PieData = {
-  chart: {
-    type: "pie",
-    options3d: {
-      enabled: true,
-      alpha: 45,
-    },
+  {
+    date: new Date(1994, 2, 23),
+    l: 26.875,
+    h: 28.125,
+    o: 27.0,
+    c: 28.0,
   },
-  title: {
-    text: "Contents of Highsoft's weekly fruit delivery",
+  {
+    date: new Date(1994, 2, 24),
+    l: 26.25,
+    h: 27.875,
+    o: 27.75,
+    c: 27.625,
   },
-  subtitle: {
-    text: "3D donut in Highcharts",
+  {
+    date: new Date(1994, 2, 25),
+    l: 27.5,
+    h: 28.75,
+    o: 27.75,
+    c: 28.0,
   },
-  plotOptions: {
-    pie: {
-      innerSize: 100,
-      depth: 45,
-    },
+  {
+    date: new Date(1994, 2, 28),
+    l: 25.75,
+    h: 28.25,
+    o: 28.0,
+    c: 27.25,
   },
-  series: [
-    {
-      name: "Delivered amount",
-      data: [
-        ["Bananas", 8],
-        ["Kiwi", 3],
-        ["Mixed nuts", 1],
-        ["Oranges", 6],
-        ["Apples", 8],
-        ["Pears", 4],
-        ["Clementines", 4],
-        ["Reddish (bag)", 1],
-        ["Grapes (bunch)", 1],
-      ],
-    },
-  ],
-};
-
-export const DrilldownData = {
-  chart: {
-    type: "column",
+  {
+    date: new Date(1994, 2, 29),
+    l: 26.375,
+    h: 27.5,
+    o: 27.5,
+    c: 26.875,
   },
-  title: {
-    text: "Basic drilldown",
+  {
+    date: new Date(1994, 2, 30),
+    l: 25.75,
+    h: 27.5,
+    o: 26.375,
+    c: 26.25,
   },
-  xAxis: {
-    type: "category",
+  {
+    date: new Date(1994, 2, 31),
+    l: 24.75,
+    h: 27.0,
+    o: 26.5,
+    c: 25.25,
   },
-  legend: {
-    enabled: false,
-  },
-  plotOptions: {
-    series: {
-      borderWidth: 0,
-      dataLabels: {
-        enabled: true,
-      },
-    },
-  },
-  series: [
-    {
-      name: "Things",
-      colorByPoint: true,
-      data: [
-        {
-          name: "Animals",
-          y: 5,
-          drilldown: "animals",
-        },
-        {
-          name: "Fruits",
-          y: 2,
-          drilldown: "fruits",
-        },
-        {
-          name: "Cars",
-          y: 4,
-          drilldown: "cars",
-        },
-      ],
-    },
-  ],
-  drilldown: {
-    series: [
-      {
-        id: "animals",
-        data: [
-          ["Cats", 4],
-          ["Dogs", 2],
-          ["Cows", 1],
-          ["Sheep", 2],
-          ["Pigs", 1],
-        ],
-      },
-      {
-        id: "fruits",
-        data: [
-          {
-            name: "Apples",
-            y: 4,
-          },
-          {
-            name: "Oranges",
-            y: 2,
-            drilldown: "third-leves",
-          },
-        ],
-      },
-      {
-        id: "cars",
-        data: [
-          ["Toyota", 4],
-          ["Opel", 2],
-          ["Volkswagen", 2],
-        ],
-      },
-      {
-        id: "third-leves",
-        data: [
-          ["Toyota", 4],
-          ["Opel", 2],
-          ["Volkswagen", 2],
-        ],
-      },
-    ],
-  },
-};
+];
