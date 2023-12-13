7963471c8b96df3ee0277f22b4e1672cbd18cd53
nklasKrmnn <kormann8@icloud.com>
2023-12-13 22:29:39 +0100

    MLP for Error detection in training process

---
+++ src_transformers/models/vanilla_mlp.py
@@ -0,0 +1,35 @@
+import math
+
+import torch
+import torch.nn as nn
+
+torch.autograd.set_detect_anomaly(True)
+
+
+class Multi_Layer_Perceptron(nn.Module):
+
+    def __init__(self,
+                 seq_len_encoder,
+                 seq_len_decoder,
+                 dim_decoder,
+                 dim_encoder,
+                 hidden_dim,
+                 device: torch.device):
+        super(Multi_Layer_Perceptron, self).__init__()
+        self.device = device
+        self.layer_stack = nn.Sequential(
+            nn.Linear(seq_len_encoder * dim_encoder, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, seq_len_decoder * dim_decoder),
+        )
+
+    def forward(self, x, _):
+        x.to(self.device)
+
+        # Flatten the input (batch_size, seq_len, dim) -> (batch_size, seq_len * dim)
+        # The volume of the stock and the encoding for start off date an end of date are still in the input
+        x = x.view(x.size(0), -1)
+        output = self.layer_stack(x)
+        return output.unsqueeze(2)
--- src_transformers/utils/viz_training.py
+++ src_transformers/utils/viz_training.py
@@ -19,14 +19,15 @@ def plot_evaluation(targets: np.array, predictions: np.array):
 
     colors = cm.tab20(range(20))
 
+    if n_target_features == 1:
+        axes = [axes]
+
     for feature_idx in range(n_target_features):
         color_idx = (feature_idx * 2) % 20
         axes[feature_idx].plot(targets[:, 0, feature_idx],
                                c=colors[color_idx], label='target')
-        # ax.plot(targets[:, 1, feature_idx], c=colors[color_idx])
         axes[feature_idx].plot(
             predictions[:, 0, feature_idx], c=colors[color_idx + 1], label='prediction')
-        # ax.plot(predictions[:, 1, feature_idx], c=colors[color_idx+1])
         axes[feature_idx].legend()
 
     return fig
@@ -51,6 +52,9 @@ def plot_loss_horizon(targets: np.array, predictions: np.array):
                              figsize=(20, n_target_features * 2))
 
     colors = cm.tab20(range(20))
+
+    if n_target_features == 1:
+        axes = [axes]
 
     for feature_idx in range(n_target_features):
         color_idx = (feature_idx * 2) % 20
