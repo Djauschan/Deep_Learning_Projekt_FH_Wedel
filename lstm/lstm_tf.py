49609c303beba5ca1654936503cc3e074fd7d43f
Umut Kurucay <101885663+umutkcay@users.noreply.github.com>
2023-12-20 13:54:46 +0100

    Update lstm_tf.py; Behebung typischer Fehler
    1. MinMaxScaling
    2. Preise als inputs
    3. Preise als outputs
    4. Falscher Datensatz split

--- lstm/lstm_tf.py
+++ lstm/lstm_tf.py
@@ -2,126 +2,124 @@
 # !pip install keras-tuner -q
 # Alle anderen Pakete sind bereits in Colab verfügbar
 
-import os
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
-import matplotlib.pyplot as plt
-import keras_tuner as kt
+from sklearn.preprocessing import StandardScaler
+import datetime
+from tensorflow.keras.models import load_model
 
-class StockPredictor:
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.scaler = MinMaxScaler(feature_range=(0, 1))
-        self.model = None
-        self.name = file_path.split('/')[-1].split('.')[0]
+def load_data(file_path):
+    data = pd.read_csv(file_path)
+    data.columns = ['DateTime', 'Open', 'High', 'Low', 'Close', 'Volume']
+    data['DateTime'] = pd.to_datetime(data['DateTime'])
+    data.set_index('DateTime', inplace=True)
+    data = data[data.index.time == pd.to_datetime('16:00').time()]
+    return data
 
-    def load_data(self):
-        data = pd.read_csv(self.file_path)
-        data.columns = ['DateTime', 'Open', 'High', 'Low', 'Close', 'Volume']
-        data['DateTime'] = pd.to_datetime(data['DateTime'])
-        data.set_index('DateTime', inplace=True)
-        data = data[data.index.time == pd.to_datetime('16:00').time()]
-        return data
+# Da wir nicht die Preise direkt vorhersagen, sondern die prozentuale Veränderung, müssen wir diese berechnen
+# Eventuell sollte statt der prozentualen Veränderung der Logarithmus der prozentualen Veränderung verwendet werden
+# weil die prozentuale Veränderung bei kleinen Werten stark schwankt
+# Ein anderer Ansatz wäre die absolute Differenz zu verwenden
+def calculate_price_difference(data):
+    data['PriceDiff'] = data['Close'].diff()
+    return data.dropna()
 
-    def preprocess_data(self, data):
-        values = data['Close'].values.reshape(-1, 1)
-        scaled_data = self.scaler.fit_transform(values)
-        return scaled_data
+def calculate_percentage_change(data):
+    data['PctChange'] = data['Close'].pct_change()
+    return data.dropna()
 
-    def create_dataset(self, data, time_step=100):
-        X, y = [], []
-        for i in range(len(data) - time_step - 1):
-            a = data[i:(i + time_step), 0]
-            X.append(a)
-            y.append(data[i + time_step, 0])
-        return np.array(X), np.array(y)
+# für prozentuale Veränderung
+def preprocess_data_for_percantage(data):
+    scaler = StandardScaler()
+    data = calculate_percentage_change(data)
+    scaled_data = scaler.fit_transform(data['PctChange'].values.reshape(-1, 1))
+    return scaled_data, scaler
 
-    def split_data(self, X, y):
-        train_size = int(len(X) * 0.80)
-        valid_test_size = len(X) - train_size
-        valid_size = test_size = int(valid_test_size / 2)
-        X_train, X_valid_test = X[0:train_size], X[train_size:len(X)]
-        y_train, y_valid_test = y[0:train_size], y[train_size:len(y)]
-        X_valid, X_test = X_valid_test[:valid_size], X_valid_test[valid_size:]
-        y_valid, y_test = y_valid_test[:valid_size], y_valid_test[valid_size:]
-        return X_train.reshape(-1, 100, 1), y_train, X_valid.reshape(-1, 100, 1), y_valid, X_test.reshape(-1, 100, 1), y_test
+# für absolute Differenz
+def preprocess_data_for_pricedifference(data):
+    scaler = StandardScaler()
+    data = calculate_price_difference(data)
+    scaled_data = scaler.fit_transform(data['PriceDiff'].values.reshape(-1, 1))
+    return scaled_data, scaler
 
-    def build_model_automated_hpt(self, hp):
-        model = tf.keras.Sequential()
-        model.add(tf.keras.layers.LSTM(
-            units=hp.Int('units', min_value=2, max_value=3, step=1),
-            return_sequences=True,
-            input_shape=(100, 1)))
-        model.add(tf.keras.layers.LSTM(units=hp.Int('units', min_value=2, max_value=3, step=1), return_sequences=False))
-        model.add(tf.keras.layers.Dense(units=hp.Int('dense_units', min_value=2, max_value=3, step=1)))
-        model.add(tf.keras.layers.Dense(1))
-        #TODO: Nur bei ARM Macs den legacy Adam optimizer nehmen.-> ansonsten ohne legacy
-        model.compile(optimizer=tf.keras.optimizers.legacy.Adam(hp.Float('learning_rate', min_value=1e-4, max_value=1e-2, sampling='LOG')), loss='mean_squared_error')
-        return model
+def create_dataset(data, time_step=30):
+    X, y = [], []
+    for i in range(len(data) - time_step):
+        X.append(data[i:(i + time_step), 0])
+        y.append(data[i + time_step, 0])
+    return np.array(X), np.array(y)
 
-    def train_model(self, X_train, y_train, X_valid, y_valid):
-        #Early Stopping
-        es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', restore_best_weights=True, verbose=1, patience=5)
-        #Automated Hyperparameter Tuning
-        tuner = kt.RandomSearch(
-            self.build_model_automated_hpt,
-            objective='val_loss',
-            #Standardwert max_trials ist 10
-            max_trials=3,
-            executions_per_trial=3,
-            directory='my_dir',
-            #TODO FileName in Abhängigkeit von der Aktie ändern
+# Modellerstellung:
+def build_model(input_shape):
+    model = tf.keras.Sequential([
+        tf.keras.layers.LSTM(50, return_sequences=True, input_shape=input_shape),
+        tf.keras.layers.LSTM(50),
+        tf.keras.layers.Dense(25),
+        tf.keras.layers.Dense(1)
+    ])
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    return model
 
-            project_name='lstm_stock_prediction_')
-        tuner.search(X_train, y_train, epochs=10, validation_data=(X_valid, y_valid))
+#Speicherung des Modells
+def save_model(model):
+    timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    model_name = f"stock_predictor_model_{timestamp}.keras"
+    model.save(model_name)
+    print(f"Modell gespeichert als: {model_name}")
 
-        best_hp = tuner.get_best_hyperparameters(num_trials=1)[0]
-        self.model = tuner.get_best_models(num_models=1)[0]
-        history = self.model.fit(X_train, y_train, epochs=50, validation_data=(X_valid, y_valid), callbacks=[es])
 
-        self.plot_loss(history)
+#Ausführung
+file_path = '/content/drive/MyDrive/data/AMD_1min.txt'
+data = load_data(file_path)
+scaled_data, scaler = preprocess_data(data)
+X, y = create_dataset(scaled_data, 30)
 
-    def predict(self, X_test):
-        test_predictions = self.model.predict(X_test)
-        test_predictions = self.scaler.inverse_transform(test_predictions)
-        return test_predictions
+train_size = int(len(X) * 0.70)
+X_train, X_valid = X[:train_size], X[train_size:]
+y_train, y_valid = y[:train_size], y[train_size:]
 
-    def plot_predictions(self, y_test, test_predictions):
-        y_test_scaled = self.scaler.inverse_transform(y_test.reshape(-1, 1))
-        mse = mean_squared_error(y_test_scaled, test_predictions)
-        plt.figure(figsize=(10,6))
-        plt.plot(y_test_scaled, label='Tatsächliche Werte', color='blue')
-        plt.plot(test_predictions, label='Vorhersagen', color='red')
-        plt.title(f'Aktienpreisvorhersage (MSE beträgt: {mse})')
-        plt.xlabel('Zeit')
-        plt.ylabel('Preis')
-        plt.legend()
-        plt.show()
+model = build_model((30, 1))
+model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_valid, y_valid))
 
-    def plot_loss(self, history):
-        plt.figure(figsize=(10, 6))
-        plt.plot(history.history['loss'], label='Trainingsverlust')
-        plt.plot(history.history['val_loss'], label='Validierungsverlust')
-        plt.title('Trainings- und Validierungsverlust')
-        plt.xlabel('Epochen')
-        plt.ylabel('Verlust')
-        plt.legend()
-        plt.show()
+# Modell speichern
+# Kommentar entfernen, wenn Modell gespeichert werden soll: 
+# save_model(model)
 
-    def save_model(self, model_name):
-        self.model.save(model_name)
+#alte predict_next_day Funktion: für prozentuale Veränderung
+# def predict_next_day(model, data, scaler, last_close_price):
+#     if len(data) < 30:
+#         raise ValueError("Nicht genügend Daten für eine Vorhersage verfügbar.")
+#     last_sequence = data[-30:].reshape(1, 30, 1)
+#     predicted_scaled = model.predict(last_sequence)
+#     predicted_pct_change = scaler.inverse_transform(predicted_scaled)[0, 0]
+#     predicted_absolute_change = last_close_price * predicted_pct_change
+#     predicted_close_price = last_close_price + predicted_absolute_change
+#     return predicted_close_price
 
-# for file in os.listdir('/Users/umutkurucay/Documents/Developer/LSTM_testing/Data'):
-#   if file.endswith('.csv'):
-predictor = StockPredictor(f'/content/drive/MyDrive/data/AMD_1min.txt')
-data = predictor.load_data()
-scaled_data = predictor.preprocess_data(data)
-X, y = predictor.create_dataset(scaled_data)
-X_train, y_train, X_valid, y_valid, X_test, y_test = predictor.split_data(X, y)
-predictor.train_model(X_train, y_train, X_valid, y_valid)
-test_predictions = predictor.predict(X_test)
-predictor.plot_predictions(y_test, test_predictions)
-predictor.save_model('LSTM_autotunedTEST.keras')
+def predict_next_day(model, data, scaler, last_close_price):
+    if len(data) < 30:
+        raise ValueError("Nicht genügend Daten für eine Vorhersage verfügbar.")
+    last_sequence = data[-30:].reshape(1, 30, 1)
+    predicted_scaled = model.predict(last_sequence)
+    predicted_price_change = scaler.inverse_transform(predicted_scaled)[0, 0]
+    predicted_close_price = last_close_price + predicted_price_change
+    return predicted_close_price
+
+# Setzen Sie dies auf "ja", wenn Sie ein zuvor gespeichertes Modell laden möchten, 
+# und auf "nein", wenn Sie das in dieser Sitzung trainierte Modell verwenden möchten.
+load_saved_model = "nein"  # Oder "ja"
+
+if load_saved_model.lower() == "ja":
+    model_filename = input("Geben Sie den Dateinamen des zu ladenden Modells ein: ")
+    model = load_model(model_filename)
+else:
+    # Hier verwenden wir das in dieser Sitzung trainierte Modell
+    print("Verwendung des in dieser Sitzung trainierten Modells")
+
+# Letzter bekannter Close-Preis
+last_close_price = data['Close'].iloc[-1]
+
+# Vorhersage des nächsten Tages
+predicted_close = predict_next_day(model, scaled_data, scaler, last_close_price)
+print(f"Vorhergesagter Close-Preis für den nächsten Tag: {predicted_close}")
