6b04726bf1d9764026a308616e1dcade3f34d8ca
Johann Beaumont <johann.beaumont@hotmail.de>
2024-01-03 14:23:06 +0100

    update Header.vue to show actual username

--- WebApp/services/frontend/src/views/Header.vue
+++ WebApp/services/frontend/src/views/Header.vue
@@ -3,19 +3,19 @@
         <div class="row align-items-center">
             <div class="d-flex justify-content-end">
                 <div v-if="isUserLoggedIn">
-                    <DropDownMenu />  
+                    <DropDownMenu />
                     <div class="extendedHeader">
-                      <img alt="image" src="/external/fh_wedel_logo.svg-200h.png" class="home-image2" />
-                      <div class="LoginPicText">
-                       <div class="LoginPic">
-                          <img alt="image" src="/profil-200h.jpg" class="home-image1" />
-                       </div>
-                       <div class="LoginText">
-                        <span class="home-text">Thomas <br></span>
-                        <span class="home-text01">View Profile</span>
-                       </div>
-                      </div>
-                   </div>
+                        <img alt="image" src="/external/fh_wedel_logo.svg-200h.png" class="home-image2" />
+                        <div class="LoginPicText">
+                            <div class="LoginPic">
+                                <img alt="image" src="/profil-200h.jpg" class="home-image1" />
+                            </div>
+                            <div class="LoginText">
+                                <span class="home-text">{{ username }} <br></span>
+                                <span class="home-text01">View Profile</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -39,6 +39,7 @@ export default defineComponent({
 
         return {
             isUserLoggedIn,
+            username: localStorage.logged_user,
         };
     },
 });
@@ -73,47 +74,52 @@ export default defineComponent({
 }
 
 .home-image2 {
-  height: 100px;
-  object-fit: cover; 
-  margin-left: 2%;
+    height: 100px;
+    object-fit: cover;
+    margin-left: 2%;
 }
 
 .home-image1 {
-  border-radius: 20px; /* Adjust the value to control the roundness of the corners */
+    border-radius: 20px;
+    /* Adjust the value to control the roundness of the corners */
 }
 
 .LoginPicText {
-  background-color: white; /* Set the background color to white */
-  width: 18vh;
-  height: 8vh;
-  object-fit: cover;
-  margin-top: 10px;
-  margin-left: auto;
-  margin-right: 5%;
-  display: flex;
-  justify-content: space-between;
-  border-color: #7e7e7e;
-  border-width: 1px;
-  padding-top: 0.5%;
-  padding-left: 0.5%;
-  padding-right: 0.5%;
-  padding-bottom: 0.5%;
-  overflow: hidden; /* Clear the float */
-  border-radius: 10px; /* Adjust the value to control the roundness of the corners */
+    background-color: white;
+    /* Set the background color to white */
+    width: 18vh;
+    height: 8vh;
+    object-fit: cover;
+    margin-top: 10px;
+    margin-left: auto;
+    margin-right: 5%;
+    display: flex;
+    justify-content: space-between;
+    border-color: #7e7e7e;
+    border-width: 1px;
+    padding-top: 0.5%;
+    padding-left: 0.5%;
+    padding-right: 0.5%;
+    padding-bottom: 0.5%;
+    overflow: hidden;
+    /* Clear the float */
+    border-radius: 10px;
+    /* Adjust the value to control the roundness of the corners */
 }
 
 .LoginPic {
     padding-top: 10px;
-    margin-right: 10px; /* Add some margin for spacing */
+    margin-right: 10px;
+    /* Add some margin for spacing */
 }
 
 .LoginText {
     padding-top: 10px;
-    overflow: hidden; /* Clear the float */
+    overflow: hidden;
+    /* Clear the float */
 }
 
 .extendedHeader {
-  display: flex;
-  justify-content: space-between;
-}
-</style>
+    display: flex;
+    justify-content: space-between;
+}</style>
