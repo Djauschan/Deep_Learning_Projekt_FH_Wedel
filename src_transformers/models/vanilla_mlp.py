c755b77ad4208536fdded824ed3ffdd3de397e05
Eira <eira.hammerich@gmx.de>
2024-01-12 19:30:31 +0100

    batch norm und residual connections implementiert

--- src_transformers/models/vanilla_mlp.py
+++ src_transformers/models/vanilla_mlp.py
@@ -5,34 +5,48 @@ import torch.nn as nn
 
 torch.autograd.set_detect_anomaly(True)
 
+class ResidualBlock(nn.Module):
+    def __init__(self, 
+                 hidden_dim):
+        super(ResidualBlock, self).__init__()
+        self.linear = nn.Linear(hidden_dim, hidden_dim)
+        self.batch_norm = nn.BatchNorm1d(hidden_dim)  # Batch Normalisierung
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+       
+        residual = x
+        out = self.linear(x)
+        out = self.relu(out)
+        out = out + residual # ursprünglichen Input zum Output addieren
+        return out
+
 
 class Multi_Layer_Perceptron(nn.Module):
-
-    def __init__(self,
-                 seq_len_encoder,
-                 seq_len_decoder,
-                 dim_decoder,
-                 dim_encoder,
-                 hidden_dim,
-                 hidden_layers,
-                 dropoutrate,
+    def __init__(self, 
+                 seq_len_encoder, 
+                 seq_len_decoder, 
+                 dim_decoder, 
+                 dim_encoder, 
+                 hidden_dim, 
+                 dropoutrate, # bei BatchNorm erstmal kein Dropout um kein Noise zu erzeugen
                  device: torch.device):
         super(Multi_Layer_Perceptron, self).__init__()
         self.device = device
+
+        # Erster Layer
         layers = [nn.Linear(seq_len_encoder * dim_encoder, hidden_dim), nn.ReLU()]
 
-        # Hidden Layers
-        for _ in range(hidden_layers):
-            layers.append(nn.Linear(hidden_dim, hidden_dim))
-            layers.append(nn.ReLU())
-            # Dropout optional:
+        # Residual Layers
+        for _ in range(6):  # 6-mal für die 6 versteckten Layer
+            layers.append(ResidualBlock(hidden_dim))
+            # Optional: Dropout einfügen
             # if dropoutrate > 0:
             #     layers.append(nn.Dropout(dropoutrate))
 
-        # Last Layer
+        # Letzter Layer
         layers.append(nn.Linear(hidden_dim, seq_len_decoder * dim_decoder))
 
-      
         self.layer_stack = nn.Sequential(*layers)
 
     def forward(self, x, _):
