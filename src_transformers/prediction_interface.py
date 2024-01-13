ed14d538feb530ba0b370090a963d78151ce580f
Luca Nickel <luca.nickel@pm.me>
2024-01-13 19:17:27 +0100

    🔥 Removed unnecessary imports and old docstrings

--- src_transformers/prediction_interface.py
+++ src_transformers/prediction_interface.py
@@ -3,7 +3,7 @@ This module contains the TransformerInterface class and the MultiSymbolDataset c
 """
 import pickle
 from dataclasses import dataclass
-from datetime import date, timedelta
+from datetime import timedelta
 from pathlib import Path
 from typing import List
 
@@ -12,37 +12,12 @@ import torch
 from torch.utils.data import DataLoader, Dataset
 
 from src_transformers.abstract_model import AbstractModel
-from src_transformers.preprocessing.csv_io import get_csv_shape, read_csv_chunk
-from src_transformers.preprocessing.data_processing import (
-    add_time_information,
-    fill_dataframe,
-    get_all_dates,
-)
-from src_transformers.preprocessing.preprocessing_constants import SCALER_OPTIONS
-from src_transformers.preprocessing.txtReader import DataReader
-from src_transformers.utils.logger import Logger
-
-# from src_transformers.preprocessing.prediction_dataset import PredictionDataset
 
 INTERVAL_MINUTES = 120
 NUM_INTERVALS = 24
 
 
 class TransformerInterface(AbstractModel):
-    """
-    A class used to make predictions using a Transformer model.
-
-    This class inherits from the AbstractModel class and overrides its methods to implement
-    functionality specific to Transformer models. It includes methods for loading data,
-    preprocessing, data, loading a model, and making predictions.
-
-    The predict method takes a start and end timestamp and an optional interval, and returns a
-    DataFrame with predicted stock prices for the given time period.
-
-    Attributes:
-        model (nn.Module): The PyTorch model to use for predictions.
-        dataset (MultiSymbolDataset): The dataset to use for predictions.
-    """
 
     def predict(
         self,
@@ -50,17 +25,6 @@ class TransformerInterface(AbstractModel):
         timestamp_end: pd.Timestamp,
         interval: int = 0,
     ) -> pd.DataFrame:
-        """predict stock price for a given time interval
-
-        Args:
-            timestamp_start (pd.Timestamp): start time of the time period
-            timestamp_end (pd.Timestamp): end time of the time period
-            interval (int, optional): interval in minutes. Defaults to 0.
-            file_path (str, optional): path to the config file. Defaults to DEFAULT_PATH.
-
-        Returns:
-            pd.DataFrame: dataframe with columns: timestamp, 1-n prices of stock_symbols
-        """
         # TODO: Pass first and last date to load_data
         # first_date = timestamp_start.strftime("%Y-%m-%d")
         # last_date = timestamp_end.strftime("%Y-%m-%d")
@@ -142,21 +106,6 @@ class TransformerInterface(AbstractModel):
 
 @dataclass
 class PredictionDataset(Dataset):
-    """
-    A PyTorch Dataset for multi-symbol financial data.
-
-    This class handles multi-symbol financial data. It supports creating a new dataset
-    from a configuration file and loading existing data from a csv file, if set to do so
-    in the configuration file. The dataset is designed to deliver data for a Transformer.
-
-    Attributes:
-        length (int): The number of samples in the dataset.
-        encoder_dimensions (int): The number of dimensions in the encoder input.
-        decoder_dimensions (int): The number of dimensions in the decoder input.
-        encoder_input_length (int): The length of the encoder input sequence.
-        decoder_input_length (int): The length of the decoder input sequence.
-        data_file (str): The path to the file to store the data in or to load the data from.
-    """
 
     data: pd.DataFrame
 
@@ -182,37 +131,9 @@ class PredictionDataset(Dataset):
         )
 
     def __len__(self) -> int:
-        """
-        Returns the number of samples in the dataset.
-
-        The number of samples is calculated as the total length of the data minus
-        the length of the encoder and decoder input sequences plus one.
-
-        Returns:
-            int: The number of samples in the dataset.
-        """
         return 1
 
     def __getitem__(self, index: int) -> torch.Tensor:
-        """
-        Returns time series sequences as input for the encoder and decoder from the dataset
-        starting at the specified index.
-
-        The encoder input sequence starts at the specified index and has a length of
-        `self.encoder_input_length`. The decoder target sequence starts immediately after the
-        input sequence and has a length of `self.decoder_target_length`.
-
-        The method reads a chunk of data from the data file, starting at `encoder_input_start`
-        and ending at `decoder_target_end`. It then extracts the encoder input and decoder target
-        from this chunk and converts them to PyTorch tensors.
-
-        Args:
-            index (int): The index of the sample in the dataset.
-
-        Returns:
-            torch.Tensor: The encoder input sequence.
-            torch.Tensor: The decoder target sequence.
-        """
         data = self.data.to_numpy()
         data = torch.tensor(data, dtype=torch.float32)
 
