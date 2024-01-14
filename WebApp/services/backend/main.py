8e50d0ccbaf4c4fa4762df7b44f7a2d1d71250b5
Luca Nickel <luca.nickel@pm.me>
2024-01-14 17:58:11 +0100

    ✨ Added transformer interface to backend (as well as all need resources)

--- WebApp/services/backend/main.py
+++ WebApp/services/backend/main.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI, HTTPException, Depends
+import datetime as DT
+
+import bcrypt
+import crud
+import models
+import pandas as pd
+import schemas
+from database import SessionLocal, engine
+from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
-import crud, models, schemas
-from database import SessionLocal, engine
-import bcrypt
-import datetime as DT
-import pandas as pd
-import os
+from transformer_interface import TransformerInterface
 
 # Create tables in the database based on the model definitions
 models.Base.metadata.create_all(bind=engine)
@@ -31,9 +34,11 @@ app.add_middleware(
 )
 
 # DB Dependency
+
+
 def get_db():
     db = SessionLocal()
-    
+
     try:
         yield db
     finally:
@@ -46,14 +51,18 @@ def get_stock_days(stock_symbol: str, days_back: int, db: Session = Depends(get_
     return crud.get_stock_days(stock_symbol=stock_symbol, n=days_back, db=db)
 
 # post method to delete table "users"
+
+
 @app.post("/deleteUsers/")
 def delete_users(db: Session = Depends(get_db)):
     db_delete_users = crud.delete_users(db)
     if db_delete_users:
         raise HTTPException(status_code=400, detail="Not Found")
-    #return crud.get_users(db=db, skip=0, limit=100)
+    # return crud.get_users(db=db, skip=0, limit=100)
 
 # post method to delete a single user from table "users" by id
+
+
 @app.post("/deleteUser/{username}")
 def delete_user(username: str, db: Session = Depends(get_db)):
     db_delete_user = crud.delete_user(db, username)
@@ -62,33 +71,41 @@ def delete_user(username: str, db: Session = Depends(get_db)):
     return {"message": "User deleted successfully", "status_code": 200}
 
 # post method to create a user into table "users"
+
+
 @app.post("/createUser/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user_email = crud.get_user_by_email(db, email=user.email)
-    db_user_username = crud.get_user_by_username(db, username = user.username)
+    db_user_username = crud.get_user_by_username(db, username=user.username)
     if db_user_email:
         raise HTTPException(status_code=400, detail="Email already registered")
     if db_user_username:
-        raise HTTPException(status_code=400, detail="Username already registered")
+        raise HTTPException(
+            status_code=400, detail="Username already registered")
     return crud.create_user(db=db, user=user)
+
 
 @app.get("/get_user/{username}")
 async def get_user(username: str, db: Session = Depends(get_db)):
     return crud.get_user_by_username(db, username)
 
+
 @app.get("/get_user/{email}")
 async def get_user(email: str, db: Session = Depends(get_db)):
     return crud.get_user_by_email(db, email)
+
 
 @app.put("/update_user/{username}")
 async def update_user(username: str, user_update: schemas.UserUpdate, db: Session = Depends(get_db)):
     updated_user = crud.update_user_by_username(db, username, user_update)
     return {"message": "User updated successfully"}
 
+
 @app.get("/getUsers/", response_model=list[schemas.User])
 def read_users(query: str = '', limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, query=query, limit=limit)
     return users
+
 
 @app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
@@ -97,21 +114,24 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
+
 @app.post("/login/")
 def user_login(login_request: schemas.LoginRequest, db: Session = Depends(get_db)):
     user = crud.get_user_by_username(db, login_request.user)
-    
+
     if not user:
         raise HTTPException(status_code=400, detail="Username does not exist")
 
     if not bcrypt.checkpw(login_request.password.encode('utf-8'), user.password):
         raise HTTPException(status_code=400, detail="Incorrect password")
     # Creating a login record after successful authentication
-    login_record = crud.create_login(db, owner_id=user.id)  # Removed schemas.LoginCreate()
+    # Removed schemas.LoginCreate()
+    login_record = crud.create_login(db, owner_id=user.id)
 
     if not login_record:
-        raise HTTPException(status_code=500, detail="Could not create login record")
-    
+        raise HTTPException(
+            status_code=500, detail="Could not create login record")
+
     return {
         "username": user.username,
         "email": user.email,
@@ -119,38 +139,52 @@ def user_login(login_request: schemas.LoginRequest, db: Session = Depends(get_db
         "is_active": user.is_active
     }
 
+
 @app.get("/getLogins/", response_model=list[schemas.Login])
 def get_logins(query: str = '', limit: int = 100, db: Session = Depends(get_db)):
     logins = crud.get_logins(db, query=query, limit=limit)
-    return logins 
+    return logins
+
 
 @app.get("/getLoginByUser_ID/", response_model=list[schemas.Login])
 def get_logins(user_id: int, db: Session = Depends(get_db)):
     logins = crud.get_logins_by_user_id(db, owner_id=user_id)
     return logins
 
+
 @app.get("/login/validation")
 def check_login(email: str, password: str, db: Session = Depends(get_db)):
     user = crud.get_user_by_email(db, email)
-    
+
     if user:
         raise HTTPException(status_code=400, detail="Email does not exist")
-    
+
+
 @app.get("/predict/transformer")
 def predict_transformer(stock_symbol: str):
-    return "predict_transformer"
+    transformer_interface = TransformerInterface()
+
+    start_date = pd.to_datetime("2021-01-04")
+    end_date = pd.to_datetime("2021-01-06")
+    prediction = transformer_interface.predict(start_date, end_date)
+
+    return prediction[f"close {stock_symbol}"]
+
 
 @app.get("/predict/cnn")
 def predict_cnn(stock_symbol: str):
     return crud.predict_cnn(stock_symbol=stock_symbol)
 
+
 @app.get("/predict/lstm")
 def predict_lstm(stock_symbol: str):
     return crud.predict_lstm(stock_symbol=stock_symbol)
 
+
 @app.get("/predict/arima")
 def predict_arima(stock_symbol: str):
     return crud.predict_arima(stock_symbol=stock_symbol)
+
 
 @app.get("/load/data")
 def load_data():
@@ -159,7 +193,5 @@ def load_data():
     start_date = pd.Timestamp("2021-01-04")
     end_date = pd.Timestamp("2021-01-06")
 
-    return crud.loadDataFromFile(start_date=start_date, end_date=end_date, rsc_completePath="../../../data/Aktien/AAPL_1min.txt", 
+    return crud.loadDataFromFile(start_date=start_date, end_date=end_date, rsc_completePath="../../../data/Aktien/AAPL_1min.txt",
                                  ALL_DATA_COLUMNS=allColumns, COLUMNS_TO_KEEP=relevantColumns)
-
-
