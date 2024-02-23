9f242f233392c78174fd9eb21e6ecd5f442487b6
AndreGa93 <andre.galczynski@gmail.com>
2024-02-23 19:18:34 +0100

    removed unnecessary variable

--- src_rl/api.py
+++ src_rl/api.py
@@ -9,8 +9,5 @@ async def root():
     return {"message": "Prediction API for the RL group :)))"}
 
 @app.get("/predict/")
-def predict_rl(stock_symbol: str = 'aapl', start_date: str = '2021-01-04', end_date: str = '2021-01-05'):
-    
-    prediction = interface.predict(stock_symbol, start_date, end_date)
-    
-    return [prediction]
+def predict_rl(stock_symbol: str = 'aapl', start_date: str = '2021-01-04', end_date: str = '2021-01-05') -> list[dict]:
+    return [interface.predict(stock_symbol, start_date, end_date)]
