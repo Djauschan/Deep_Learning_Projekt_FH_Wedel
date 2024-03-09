146acc0453c97191da3cefb47f0f0864b6a70d10
Luca Nickel <luca.nickel@pm.me>
2024-03-09 16:21:02 +0100

    ✨ 🔥 Remove end_date parameter from prediction routes, include end_date calculation method
    Co-authored-by: ThomasParchmann <150282079+ThomasParchmann@users.noreply.github.com>
    Co-authored-by: Philipp Duwe <44848918+PhilippDuwe@users.noreply.github.com>
    Co-authored-by: AndreGa93 <andre.galczynski@gmail.com>

--- WebApp/services/backend/main.py
+++ WebApp/services/backend/main.py
@@ -4,6 +4,7 @@ import models
 import pandas as pd
 import requests
 import schemas
+from abstract_model import resolution as resolution_enum
 from database import SessionLocal, engine
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
@@ -20,8 +21,8 @@ origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
     "http://localhost",
-    "http://localhost:8080", # Frontend
-    "http://localhost:8000", # Backend
+    "http://localhost:8080",  # Frontend
+    "http://localhost:8000",  # Backend
 ]
 
 app.add_middleware(
@@ -33,6 +34,8 @@ app.add_middleware(
 )
 
 # DB Dependency
+
+
 def get_db():
     db = SessionLocal()
     try:
@@ -41,11 +44,15 @@ def get_db():
         db.close()
 
 # test method to get Stock data for n days
+
+
 @app.get("/getStock/")
 def get_stock_days(stock_symbol: str, days_back: int, db: Session = Depends(get_db)):
     return crud.get_stock_days(stock_symbol=stock_symbol, n=days_back, db=db)
 
 # post method to delete table "users"
+
+
 @app.post("/deleteUsers/")
 def delete_users(db: Session = Depends(get_db)):
     db_delete_users = crud.delete_users(db)
@@ -54,6 +61,8 @@ def delete_users(db: Session = Depends(get_db)):
     # return crud.get_users(db=db, skip=0, limit=100)
 
 # post method to delete a single user from table "users" by id
+
+
 @app.post("/deleteUser/{username}")
 def delete_user(username: str, db: Session = Depends(get_db)):
     db_delete_user = crud.delete_user(db, username)
@@ -62,6 +71,8 @@ def delete_user(username: str, db: Session = Depends(get_db)):
     return {"message": "User deleted successfully", "status_code": 200}
 
 # post method to create a user into table "users"
+
+
 @app.post("/createUser/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user_email = crud.get_user_by_email(db, email=user.email)
@@ -73,32 +84,40 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
             status_code=400, detail="Username already registered")
     return crud.create_user(db=db, user=user)
 
+
 @app.get("/get_user/{username}")
 async def get_user(username: str, db: Session = Depends(get_db)):
     return crud.get_user_by_username(db, username)
+
 
 @app.get("/get_budget/{username}")
 async def get_budget(username: str, db: Session = Depends(get_db)):
     return crud.get_budget_by_username(db, username)
 
+
 @app.get("/get_user/{email}")
 async def get_user(email: str, db: Session = Depends(get_db)):
     return crud.get_user_by_email(db, email)
+
 
 @app.put("/update_user/{username}")
 async def update_user(username: str, user_update: schemas.UserUpdate, db: Session = Depends(get_db)):
     updated_user = crud.update_user_by_username(db, username, user_update)
     return {"message": "User updated successfully"}
 
+
 @app.put("/update_budget/{username}")
 async def update_budget_by_user(username: str, budgetInput: int, db: Session = Depends(get_db)):
-    updated_budget = crud.update_budget_by_user(db=db, username=username, new_budget=budgetInput)
+    updated_budget = crud.update_budget_by_user(
+        db=db, username=username, new_budget=budgetInput)
     return {"message": "Budget updated successfully"}
+
 
 @app.get("/getUsers/", response_model=list[schemas.User])
 def read_users(query: str = '', limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, query=query, limit=limit)
     return users
+
 
 @app.get("/users/{user_id}", response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
@@ -106,6 +125,7 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
+
 
 @app.post("/login/")
 def user_login(login_request: schemas.LoginRequest, db: Session = Depends(get_db)):
@@ -131,15 +151,18 @@ def user_login(login_request: schemas.LoginRequest, db: Session = Depends(get_db
         "is_active": user.is_active
     }
 
+
 @app.get("/getLogins/", response_model=list[schemas.Login])
 def get_logins(query: str = '', limit: int = 100, db: Session = Depends(get_db)):
     logins = crud.get_logins(db, query=query, limit=limit)
     return logins
 
+
 @app.get("/getLoginByUser_ID/", response_model=list[schemas.Login])
 def get_logins(user_id: int, db: Session = Depends(get_db)):
     logins = crud.get_logins_by_user_id(db, owner_id=user_id)
     return logins
+
 
 @app.get("/login/validation")
 def check_login(email: str, password: str, db: Session = Depends(get_db)):
@@ -148,12 +171,28 @@ def check_login(email: str, password: str, db: Session = Depends(get_db)):
     if user:
         raise HTTPException(status_code=400, detail="Email does not exist")
 
+
+def calculate_end_date(start_date: str, resolution: str):
+    if resolution == "D":
+        return (pd.Timestamp(start_date) + pd.Timedelta(days=30)).strftime("%Y-%m-%d")
+    elif resolution == "H":
+        return (pd.Timestamp(start_date) + pd.Timedelta(days=2)).strftime("%Y-%m-%d")
+    elif resolution == "M":
+        return start_date
+    else:
+        raise ValueError("Invalid resolution")
+
+
 @app.get("/predict/transformer")
-def predict_transformer(stock_symbols: str = "[AAPL, NVDA]", start_date: str = '2021-01-04', end_date: str = '2021-01-05', resolution: str = 'H'):
+def predict_transformer(stock_symbols: str = "[AAPL, NVDA]",
+                        start_date: str = "2021-01-04",
+                        resolution: str = "D"):
+    end_date = calculate_end_date(start_date, resolution)
     data_to_send = {"stock_symbols": stock_symbols,
                     "start_date": start_date,
                     "end_date": end_date,
                     "resolution": resolution}
+
     api_url = "http://predict_transformer:8000/predict"
     response = requests.get(api_url, params=data_to_send)
 
@@ -165,13 +204,17 @@ def predict_transformer(stock_symbols: str = "[AAPL, NVDA]", start_date: str = '
 
     return response.json()
 
+
 @app.get("/predict/cnn")
-def predict_cnn(stock_symbol: str = "[AAPL, NVDA]", start_date: str = '2021-01-04', end_date: str = '2021-01-05', resolution: str = 'H'):
-    data_to_send = {"stock_symbol": stock_symbol,
+def predict_cnn(stock_symbols: str = "[AAPL, NVDA]",
+                start_date: str = "2021-01-04",
+                resolution: str = "D"):
+    end_date = calculate_end_date(start_date, resolution)
+    data_to_send = {"stock_symbol": stock_symbols,
                     "start_date": start_date,
                     "end_date": end_date,
                     "resolution": resolution}
-    
+
     api_url = "http://predict_cnn:8000/predict"
     response = requests.get(api_url, params=data_to_send)
 
@@ -188,18 +231,20 @@ def predict_cnn(stock_symbol: str = "[AAPL, NVDA]", start_date: str = '2021-01-0
 #     lstm = LstmInterface()
 #     return lstm.predict('2021-01-04', '2021-01-06', 120)
 
+
 @app.get("/predict/randomForest")
-def predict_randomForest(stock_symbols: str = "[AAPL, NVDA]", start_date: str = '2021-01-04', end_date: str = '2021-01-06', resolution: str = "H"):
-    if(resolution != "D"):
+def predict_randomForest(stock_symbols: str = "[AAPL, NVDA]",
+                         start_date: str = "2021-01-04",
+                         resolution: str = "D"):
+    end_date = calculate_end_date(start_date, resolution)
+    if resolution != "D":
         start_date += " 10:00:00"
-        end_date += " 14:00:00"
+        end_date += " 16:00:00"
 
     data_to_send = {"stock_symbols": stock_symbols,
                     "start_date": start_date,
                     "end_date": end_date,
                     "resolution": resolution}
-    
-    print(data_to_send)
 
     api_url = "http://predict_ann:8000/predict/randomForest"
     response = requests.get(api_url, params=data_to_send)
@@ -212,18 +257,20 @@ def predict_randomForest(stock_symbols: str = "[AAPL, NVDA]", start_date: str = 
 
     return response.json()
 
+
 @app.get("/predict/gradientBoost")
-def predict_gradientBoost(stock_symbols: str = "[AAPL, NVDA]", start_date: str = '2021-01-04', end_date: str = '2021-01-06', resolution: str = "H"):
-    if(resolution != "D"):
+def predict_gradientBoost(stock_symbols: str = "[AAPL, NVDA]",
+                          start_date: str = "2021-01-04",
+                          resolution: str = "D"):
+    end_date = calculate_end_date(start_date, resolution)
+    if resolution != "D":
         start_date += " 10:00:00"
-        end_date += " 14:00:00"
+        end_date += " 16:00:00"
 
     data_to_send = {"stock_symbols": stock_symbols,
                     "start_date": start_date,
                     "end_date": end_date,
                     "resolution": resolution}
-    
-    print(data_to_send)
 
     api_url = "http://predict_ann:8000/predict/gradientBoost"
     response = requests.get(api_url, params=data_to_send)
@@ -236,8 +283,9 @@ def predict_gradientBoost(stock_symbols: str = "[AAPL, NVDA]", start_date: str =
 
     return response.json()
 
+
 @app.get("/predict/rl")
-def predict_rl(stock_symbols: str, start_date: str, end_date: str, resolution: str):
+def predict_rl(stock_symbols: str, start_date: str, resolution: str):
     """Predicts trading actions for a given stock symbol and time frame with every avialible model.
 
     Args:
@@ -282,6 +330,7 @@ def predict_rl(stock_symbols: str, start_date: str, end_date: str, resolution: s
 
     return random_return
 
+
 @app.get("/load/data")
 def load_data(stock_symbols: str = "[AAPL, NVDA]", start_date: str = '2021-01-04', end_date: str = '2021-01-06', resolution: str = "H"):
     allColumns = ["DateTime", "Open", "Close", "High", "Low", "a"]
@@ -297,7 +346,6 @@ def load_data(stock_symbols: str = "[AAPL, NVDA]", start_date: str = '2021-01-04
                                  COLUMNS_TO_KEEP=relevantColumns)
 
     return data
-
 
 
 """ Outdated code from the original main.py"""
