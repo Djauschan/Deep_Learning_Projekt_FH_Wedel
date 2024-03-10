67758bd20e0fed7294c6aa308f60d18fdb6db346
unknown <aykan.gueney@gmail.com>
2024-03-10 18:20:03 +0100

    Added new models

--- CNN/executer_exe.py
+++ CNN/executer_exe.py
@@ -19,7 +19,7 @@ class executer:
         """CONFIG_PATH1 = "C:\\Projekte\\__PorjectDeepLearningMain\\Deep_Learning\\CNN\\configs\\preprocessing\\dataCreating_dayTrayding_1.yml"
         preProcessingService = Preprocessor(CONFIG_PATH1)
         print('BUILD ALL DATA with CONFIG_PATH 1')
-        """
+
         SCONFIG_PATH1 = "C:\\Projekte\\__PorjectDeepLearningMain\\Deep_Learning\\CNN\\configs\\preprocessing\\dataCreating_swingTrayding_1.yml"
         preProcessingService = Preprocessor(SCONFIG_PATH1)
         print('SWING 1 -> BUILD ALL DATA with CONFIG_PATH 2')
@@ -28,11 +28,12 @@ class executer:
         print('SWING 2 -> BUILD ALL DATA with CONFIG_PATH 2')
         SCONFIG_PATH3 = "C:\\Projekte\\__PorjectDeepLearningMain\\Deep_Learning\\CNN\\configs\\preprocessing\\dataCreating_swingTrayding_3.yml"
         preProcessingService = Preprocessor(SCONFIG_PATH3)
-        print('SWING 3 -> BUILD ALL DATA with CONFIG_PATH 2')
+        print('SWING 3 -> BUILD ALL DATA with CONFIG_PATH 2')"""
         SCONFIG_PATH4 = "C:\\Projekte\\__PorjectDeepLearningMain\\Deep_Learning\\CNN\\configs\\preprocessing\\dataCreating_swingTrayding_4.yml"
         preProcessingService = Preprocessor(SCONFIG_PATH4)
         print('SWING 4 -> BUILD ALL DATA with CONFIG_PATH 2')
         ###############################
+        """
         LCONFIG_PATH1 = "C:\\Projekte\\__PorjectDeepLearningMain\\Deep_Learning\\CNN\\configs\\preprocessing\\dataCreating_longTrayding_1.yml"
         preProcessingService = Preprocessor(LCONFIG_PATH1)
         print('LONG 1 -> BUILD ALL DATA with CONFIG_PATH 1')
@@ -51,7 +52,7 @@ class executer:
 
         LCONFIG_PATH5 = "C:\\Projekte\\__PorjectDeepLearningMain\\Deep_Learning\\CNN\\configs\\preprocessing\\dataCreating_longTrayding_5.yml"
         preProcessingService = Preprocessor(LCONFIG_PATH5)
-        print('LONG 4 -> BUILD ALL DATA with CONFIG_PATH 4')
+        print('LONG 4 -> BUILD ALL DATA with CONFIG_PATH 4')"""
 
     def startTraining_w(self):
         """
@@ -62,7 +63,7 @@ class executer:
         trainExe.exe()
         print('END 1: train & export Model')
         time.sleep(600)
-        """
+
 
         SWING1_CONFIG_PATH = "C:\\Projekte\\__PorjectDeepLearningMain\\Deep_Learning\\CNN\\configs\\training\\baseModel_SwingTrading_1.yml"
         #start 12:50
@@ -77,7 +78,7 @@ class executer:
         trainExe = CNN_TimeSeriesModel(importer)
         trainExe.exe()
         print('END 1: train & export Model')
-        time.sleep(300)
+        time.sleep(300)"""
         SWING3_CONFIG_PATH = "C:\\Projekte\\__PorjectDeepLearningMain\\Deep_Learning\\CNN\\configs\\training\\baseModel_SwingTrading_3.yml"
         #start 12:50
         importer: Importer = Importer(SWING3_CONFIG_PATH)
@@ -92,34 +93,14 @@ class executer:
         trainExe.exe()
         print('END 1: train & export Model')
 
-        SWING1_CONFIG_PATH = "C:\\Projekte\\__PorjectDeepLearningMain\\Deep_Learning\\CNN\\configs\\training\\baseModel_Langfristig_1.yml"
+        SWING1_CONFIG_PATH = "C:\\Projekte\\__PorjectDeepLearningMain\\Deep_Learning\\CNN\\configs\\training\\baseModel_Langfristig_5.yml"
         #start 12:50
         importer: Importer = Importer(SWING1_CONFIG_PATH)
         trainExe = CNN_TimeSeriesModel(importer)
         trainExe.exe()
         print('END 1: train & export Model')
-        time.sleep(300)
-        SWING2_CONFIG_PATH = "C:\\Projekte\\__PorjectDeepLearningMain\\Deep_Learning\\CNN\\configs\\training\\baseModel_Langfristig_2.yml"
-        #start 12:50
-        importer: Importer = Importer(SWING2_CONFIG_PATH)
-        trainExe = CNN_TimeSeriesModel(importer)
-        trainExe.exe()
-        time.sleep(300)
-        print('END 2: train & export Model')
-        SWING3_CONFIG_PATH = "C:\\Projekte\\__PorjectDeepLearningMain\\Deep_Learning\\CNN\\configs\\training\\baseModel_Langfristig_3.yml"
-        # start 12:50
-        importer: Importer = Importer(SWING3_CONFIG_PATH)
-        trainExe = CNN_TimeSeriesModel(importer)
-        trainExe.exe()
-        time.sleep(300)
-        print('END 3: train & export Model')
-        SWING4_CONFIG_PATH = "C:\\Projekte\\__PorjectDeepLearningMain\\Deep_Learning\\CNN\\configs\\training\\baseModel_Langfristig_4.yml"
-        # start 12:50
-        importer: Importer = Importer(SWING4_CONFIG_PATH)
-        trainExe = CNN_TimeSeriesModel(importer)
-        trainExe.exe()
-        print('END 4: train & export Model')
+
 
 exe = executer()
-#exe.buildTimeSeriesData_w()
-exe.startTraining_w()
+exe.buildTimeSeriesData_w()
+#exe.startTraining_w()
